cbbf82e46c580c5891b4ed4f7666d7893028f3d4
Per Odlund <per.odlund@gmail.com>
2021-10-21 11:45:39 +0200

    Add telescope search title function

--- lua/zk.lua
+++ lua/zk.lua
@@ -1,3 +1,5 @@
+local sqlite = require('sqlite')
+
 local M = {}
 
 -- as for now, don't configure lsp, this is just above all that
@@ -7,7 +9,7 @@ M.settings = {
 
 -- Might merge tables in the future
 function M.Setup(setting)
-	m.settings = setting
+	M.settings = setting
 end
 
 function M.zkNew(bufnr, title)
@@ -28,15 +30,15 @@ end
 
 function M.zkAsk(bufnr)
 	local title = vim.fn.input("Zk Title: ")
-	m.zkNew(bufnr, title)
+	M.zkNew(bufnr, title)
 end
 
 function M.zkSnap(title)
 	local result
 	if title then
-		result = vim.fn.execute("silent !zk new -W " .. m.settings.path .. " -p -t " .. title)
+		result = vim.fn.execute("silent !zk new -W " .. M.settings.path .. " -p -t " .. title)
 	else
-		result = vim.fn.execute("silent !zk new -W " .. m.settings.path .. " -p")
+		result = vim.fn.execute("silent !zk new -W " .. M.settings.path .. " -p")
 	end
 	local trimmed = string.gsub(result, ".*\r", "")
 	vim.api.nvim_command('topleft new ' .. trimmed)
@@ -60,8 +62,86 @@ function M.zkIndex(bufnr)
 	end)
 end
 
+-- raw search!
+function M.search_raw()
+	require ("telescope.builtin").live_grep({
+		prompt_title = "Zk",
+		cwd = M.settings.path,
+	})
+end
+
+local pickers = require "telescope.pickers"
+local finders = require "telescope.finders"
+local conf = require("telescope.config").values
+local actions = require "telescope.actions"
+local action_state = require "telescope.actions.state"
+local putils = require "telescope.previewers.utils"
+local previewers = require "telescope.previewers"
+
+function M.search_title(opts)
+	-- maybe move this to oneshot_job
+	local path = M.settings.path .. "/.zk/notebook.db"
+	local titles = sqlite.with_open(path, function(db)
+		return db:select("notes", {select = {"title", "path", "raw_content"}})
+	end)
+
+	pickers.new(opts, {
+		prompt_title = "Zk titles",
+		finder = finders.new_table {
+			results = titles,
+			entry_maker = function(entry)
+				return {
+					value = entry,
+					display = entry.title,
+					ordinal = entry.title,
+				}
+			end
+		},
+		sorter = conf.generic_sorter(opts),
+		previewer = previewers.new_buffer_previewer {
+			title = "Zk preview",
+			keep_last_buf = true,
+			define_preview = function(self, entry)
+					vim.api.nvim_buf_set_lines(self.state.bufnr, 0, -1, false, vim.split(entry.value.raw_content, "\n"))
+					putils.highlighter(self.state.bufnr, "markdown")
+			end,
+		},
+		-- feels a bit copy-pasty atm
+		attach_mappings = function(prompt_bufnr, map)
+			actions.select_default:replace(function()
+				actions.close(prompt_bufnr)
+				local selection = action_state.get_selected_entry()
+				local fpath = M.settings.path .. "/" .. selection.value.path
+				vim.cmd(":e " .. fpath)
+			end)
+			actions.select_horizontal:replace(function()
+				actions.close(prompt_bufnr)
+				local selection = action_state.get_selected_entry()
+				local fpath = M.settings.path .. "/" .. selection.value.path
+				vim.cmd(":split " .. fpath)
+			end)
+			actions.select_vertical:replace(function()
+				actions.close(prompt_bufnr)
+				local selection = action_state.get_selected_entry()
+				local fpath = M.settings.path .. "/" .. selection.value.path
+				vim.cmd(":vs " .. fpath)
+			end)
+			actions.select_tab:replace(function()
+				actions.close(prompt_bufnr)
+				local selection = action_state.get_selected_entry()
+				local fpath = M.settings.path .. "/" .. selection.value.path
+				vim.cmd(":tabe " .. fpath)
+			end)
+			return true
+		end,
+	  }):find()
+end
+
+function M.search_tag()
+end
 
 
+M.search_title()
 
 -- TODO telescope functions etc
 
